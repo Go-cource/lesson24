c52ef853a604fc138f6a1bbbb6c8bd2b9ed05ce1
Fisherman August <fshrmn.august@gmail.com>
2025-12-28 12:59:20 +0300

    consumer

--- consumer.go
+++ consumer.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"log"
 	"time"
 
@@ -15,8 +16,17 @@ const (
 )
 
 func readMsg(ctx context.Context, r *kafka.Reader) {
+	for {
+		msg, err := r.ReadMessage(ctx)
+		if err != nil {
+			fmt.Println(err)
+			continue
+		}
+		fmt.Printf("I got message: %s:%s (topic: %v, partition: %v, offset: %v)\n", string(msg.Key), string(msg.Value), msg.Topic, msg.Partition, msg.Offset)
 
+	}
 }
+
 func main() {
 	r := kafka.NewReader(kafka.ReaderConfig{
 		Brokers: []string{kafkaAddress},
