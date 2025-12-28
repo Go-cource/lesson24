62fc9997f57492ccf93b65a271531b166abbe434
Fisherman August <fshrmn.august@gmail.com>
2025-12-28 12:53:12 +0300

    consumer main

--- consumer.go
+++ consumer.go
@@ -1,5 +1,33 @@
 package main
 
-func main(){
-	
+import (
+	"context"
+	"log"
+	"time"
+
+	"github.com/segmentio/kafka-go"
+)
+
+const (
+	topic        = "lesson24Topic"
+	kafkaAddress = "localhost:9092"
+	group        = "lesson24Group"
+)
+
+func readMsg(ctx context.Context, r *kafka.Reader) {
+
+}
+func main() {
+	r := kafka.NewReader(kafka.ReaderConfig{
+		Brokers: []string{kafkaAddress},
+		Topic:   topic,
+		GroupID: group,
+	})
+	defer r.Close()
+
+	log.Println("Consumer connected...")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 1*time.Second)
+	defer cancel()
+	readMsg(ctx, r)
 }
