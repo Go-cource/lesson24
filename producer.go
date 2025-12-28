f51d4ffd688eb252a7df773fa08a4614c88c2746
Fisherman August <fshrmn.august@gmail.com>
2025-12-28 12:47:06 +0300

    producer

--- producer.go
+++ producer.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
+	"time"
 
 	"github.com/segmentio/kafka-go"
 )
@@ -13,7 +15,23 @@ const (
 )
 
 func sendMsgToKafka(w *kafka.Writer, messages []string) {
+	for i, msg := range messages {
+		oneMessage := kafka.Message{
+			Key:   []byte(fmt.Sprintf("Message #%d", i+1)),
+			Value: []byte(msg),
+		}
 
+		ctx, cancel := context.WithCancel(context.Background())
+		defer cancel()
+
+		err := w.WriteMessages(ctx, oneMessage)
+		if err != nil {
+			fmt.Println("Error with kafka writer")
+		} else {
+			fmt.Printf("Message #%d sended to kafka\n", i+1)
+		}
+		time.Sleep(2 * time.Second)
+	}
 }
 
 func main() {
@@ -30,6 +48,6 @@ func main() {
 	}
 	sendMsgToKafka(w, messages)
 	log.Println("Producer finished...")
-	log.Println("Happy New Year...")
+	log.Println("Happy New Year!")
 	fmt.Println("🎄")
 }
