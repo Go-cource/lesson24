72f364677c3e2a19ddddcd405539298d6fd2e9e2
Fisherman August <fshrmn.august@gmail.com>
2025-12-28 12:37:42 +0300

    main in producer

--- producer.go
+++ producer.go
@@ -1,5 +1,35 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"log"
 
+	"github.com/segmentio/kafka-go"
+)
+
+const (
+	topic        = "lesson24Topic"
+	kafkaAddress = "localhost:9092"
+)
+
+func sendMsgToKafka(w *kafka.Writer, messages []string) {
+
+}
+
+func main() {
+	w := kafka.NewWriter(kafka.WriterConfig{
+		Brokers: []string{kafkaAddress},
+		Topic:   topic,
+	})
+	defer w.Close()
+	messages := []string{
+		"Last Christmas",
+		"I gave u my heart",
+		"But in very next day",
+		"U gave it away",
+	}
+	sendMsgToKafka(w, messages)
+	log.Println("Producer finished...")
+	log.Println("Happy New Year...")
+	fmt.Println("🎄")
 }
